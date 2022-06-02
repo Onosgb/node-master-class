dea71fc34269e9fdbf3f1db83ac00ab60a891261
Onosgb <onosgb@gmail.com>
2022-06-02 16:15:14 +0100

    Get the payload, if any, decoded the payload, and determine if decoding has ended

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const port = process.env.PORT | 3500;
 const url = require("url");
-
+const StringDecoder = require("string_decoder").StringDecoder;
 (async () => {
   // the server should respond to all request with a string
   const server = http.createServer((req, res) => {
@@ -17,21 +17,26 @@ const url = require("url");
 
     // Get the http Method;
     const method = req.method.toLowerCase();
-    // SEnd the response
 
-    res.end("Hello Word\n");
+    // Get the headers as an object
+    const headers = req.headers;
 
-    // log the request
-    console.log(
-      "Request received on path: " +
-        trimedpath +
-        "with this method: " +
-        method +
-        " with this query string parameters",
-      queryStringObject
-    );
-    // res.setHeader("Content-Type", "application/json");
-    // res.end();
+    // Get the payload, if any
+    const decoder = new StringDecoder("utf-8");
+    let buffer = "";
+    req.on("data", function (data) {
+      buffer += decoder.write(data);
+    });
+
+    req.on("end", function () {
+      buffer += decoder.end();
+
+      // Send the response
+      res.end("Hello Word\n");
+
+      // log headers
+      console.log("Request received with this payload: ", buffer);
+    });
   });
 
   server.listen(port, () => console.log(`listenining on port: ${port}`));
