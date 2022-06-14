77c4d9469f9471acb65bb02075f78ee96517399f
Onosgb <onosgb@gmail.com>
2022-06-14 12:07:07 +0100

    Routing Request"

--- app/index.js
+++ app/index.js
@@ -21,9 +21,13 @@ const StringDecoder = require("string_decoder").StringDecoder;
     // Get the headers as an object
     const headers = req.headers;
 
+    // Define the handlers
+    let handlers = Object.create({});
+
     // Get the payload, if any
     const decoder = new StringDecoder("utf-8");
     let buffer = "";
+
     req.on("data", function (data) {
       buffer += decoder.write(data);
     });
@@ -31,11 +35,56 @@ const StringDecoder = require("string_decoder").StringDecoder;
     req.on("end", function () {
       buffer += decoder.end();
 
-      // Send the response
-      res.end("Hello Word\n");
+      // Choose the handler the request should go to, if one is not found use the not found handler;
+      let chosenHandler =
+        typeof router[trimedpath] != "undefined"
+          ? router[trimedpath]
+          : handlers.notfound;
 
-      // log headers
-      console.log("Request received with this payload: ", buffer);
+      // Construct the data object to send to the handler
+
+      let data = {
+        trimedpath,
+        queryStringObject,
+        method,
+        headers,
+        payload: buffer,
+      };
+
+      // Sample handler
+      handlers.sample = function (data, callback) {
+        // Callback a http status code, and a payload object
+        callback(406, { name: "sample handler" });
+      };
+
+      // Route the request to the handler spicified in the handler
+      chosenHandler(data, function (statusCode, payload) {
+        // Use the status code called by the handler, or default;
+        statusCode = typeof statusCode == "number" ? statusCode : 200;
+
+        // Use the payload callback back by the handler, or default to empty object
+        payload = typeof payload === "object" ? payload : {};
+
+        // Convert the payload to a string;
+        let payloadStr = JSON.stringify(payload);
+
+        // Return the response
+        res.writeHead(statusCode);
+
+        res.end(payloadStr);
+        // log headers
+        console.log("We're returning this response", statusCode.payloadStr);
+      });
+
+      // Not found handler
+      handlers.notfound = function (data, callback) {
+        callback(404);
+      };
+
+      // Define a request routers
+      let router = {
+        sample: handler.sample,
+      };
     });
   });
 
