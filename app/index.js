0b72ef4908aeb6a1ed2322278df0a423dd53f91b
Onosgb <onosgb@gmail.com>
2022-06-29 21:17:05 +0100

    Routing Requests

--- app/index.js
+++ app/index.js
@@ -6,11 +6,11 @@ const StringDecoder = require("string_decoder").StringDecoder;
   // the server should respond to all request with a string
   const server = http.createServer((req, res) => {
     // Get the URL and parse it
-    let parseUrl = url.parse(req.url, true);
+    var parseUrl = url.parse(req.url, true);
 
     // Get the path
-    let path = parseUrl.pathname;
-    let trimedpath = path.replace(/^\/+|\/+$/g, "");
+    var path = parseUrl.pathname;
+    var trimedpath = path.replace(/^\/+|\/+$/g, "");
 
     // Get the query string as object
     const queryStringObject = parseUrl.query;
@@ -21,12 +21,9 @@ const StringDecoder = require("string_decoder").StringDecoder;
     // Get the headers as an object
     const headers = req.headers;
 
-    // Define the handlers
-    let handlers = Object.create({});
-
     // Get the payload, if any
     const decoder = new StringDecoder("utf-8");
-    let buffer = "";
+    var buffer = "";
 
     req.on("data", function (data) {
       buffer += decoder.write(data);
@@ -36,25 +33,18 @@ const StringDecoder = require("string_decoder").StringDecoder;
       buffer += decoder.end();
 
       // Choose the handler the request should go to, if one is not found use the not found handler;
-      let chosenHandler =
+      var chosenHandler =
         typeof router[trimedpath] != "undefined"
           ? router[trimedpath]
           : handlers.notfound;
 
       // Construct the data object to send to the handler
-
-      let data = {
+      var data = {
         trimedpath,
         queryStringObject,
         method,
         headers,
         payload: buffer,
-      };
-
-      // Sample handler
-      handlers.sample = function (data, callback) {
-        // Callback a http status code, and a payload object
-        callback(406, { name: "sample handler" });
       };
 
       // Route the request to the handler spicified in the handler
@@ -64,29 +54,36 @@ const StringDecoder = require("string_decoder").StringDecoder;
 
         // Use the payload callback back by the handler, or default to empty object
         payload = typeof payload === "object" ? payload : {};
-
         // Convert the payload to a string;
-        let payloadStr = JSON.stringify(payload);
+        var payloadStr = JSON.stringify(payload);
 
         // Return the response
         res.writeHead(statusCode);
 
         res.end(payloadStr);
         // log headers
-        console.log("We're returning this response", statusCode.payloadStr);
+        console.log("Returning the response", statusCode.payloadStr);
       });
-
-      // Not found handler
-      handlers.notfound = function (data, callback) {
-        callback(404);
-      };
-
-      // Define a request routers
-      let router = {
-        sample: handler.sample,
-      };
     });
   });
 
   server.listen(port, () => console.log(`listenining on port: ${port}`));
+  // Define the handlers
+  var handlers = {};
+
+  // Sample handler
+  handlers.sample = function (data, callback) {
+    // Callback a http status code, and a payload object
+    callback(406, { name: "sample handler" });
+  };
+
+  // Not found handler
+  handlers.notfound = function (data, callback) {
+    callback(404);
+  };
+
+  // Define a request router
+  var router = {
+    sample: handlers.sample,
+  };
 })();
